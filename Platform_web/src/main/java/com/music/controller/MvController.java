26fce1684fa7f55202593389f2c1b36d2265d622
Kfkcome <killzion666@gmail.com>
2023-02-22 20:03:18 +0800

    增加实现mv视频文件的传输

--- Platform_web/src/main/java/com/music/controller/MvController.java
+++ Platform_web/src/main/java/com/music/controller/MvController.java
@@ -2,6 +2,7 @@ package com.music.controller;
 
 import com.music.domain.Mv;
 import com.music.domain.ResponseResult;
+import com.music.domain.Song;
 import com.music.service.MvService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
@@ -26,5 +29,27 @@ public class MvController {
     public ResponseResult<Mv> findMvByID(@RequestBody Mv mv)
     {
         return new ResponseResult<>(200,"传输成功",mvService.findMvByID(mv.getMvID()));
+    }
+    @RequestMapping  ("/load")
+    public void SongFileLoad(HttpServletResponse response, @RequestBody Mv mv)throws IOException
+    {
+        File mvLocal=new File(mvService.findMvByID(mv.getMvID()).getAddress());
+        InputStream ips=new BufferedInputStream(new FileInputStream(mvLocal));
+        OutputStream ops=new BufferedOutputStream(response.getOutputStream());
+        // 一次性取多少个字节
+        byte[] bytes = new byte[1024];
+        // 用来接收读取的字节数组
+        // 读取到的字节数组长度，为-1时表示没有数据
+        int length = 0;
+        // 循环取数据
+        while ((length = ips.read(bytes)) != -1) {
+            // 将读取的内容转换成字符串
+            ops.write(bytes);
+        }
+        //将字节数组传输
+        ops.flush();
+        // 关闭流
+        ips.close();
+        ops.close();
     }
 }
